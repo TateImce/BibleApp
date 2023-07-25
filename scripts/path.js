63209218f96fe9f647e914c4e718770a513535f3
Immanuel Hamhola <immanuelhamhola@gmail.com>
2023-07-25 09:38:31 +0200

    Converted links to github path

--- scripts/path.js
+++ scripts/path.js
@@ -1,7 +1,7 @@
 // The following script obviously deals with our links
 
-var path = "http://bibleapp/"
-// var path = "https://tateimce.github.io/bibleapp/"
+// var path = "http://bibleapp/"
+var path = "https://tateimce.github.io/bibleapp/"
 // var path = "";
 
 var head = document.head;
